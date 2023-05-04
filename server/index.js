c3d1487e295974991b8a23b78361790124054015
santiR22 <santiagorodas688@gmail.com>
2023-05-04 15:33:12 -0300

    added delpoyment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-import express, { urlencoded } from 'express';
-import cors from 'cors';
-import { PORT } from './config.js';
-import indexRoutes from './routes/index.routes.js';
-import taskRoutes from './routes/tasks.routes.js';
+import express, { urlencoded } from "express";
+import cors from "cors";
+import { PORT } from "./config.js";
+import { fileURLToPath } from "url";
+import indexRoutes from "./routes/index.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
+import { dirname, join } from "path";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //middlewares...
 app.use(cors());
@@ -13,6 +16,8 @@ app.use(express.json());
 //routes...
 app.use(indexRoutes);
 app.use(taskRoutes);
+
+app.use(express.static(join(__dirname, "../client/dist")));
 
 app.listen(PORT);
 console.log(`server on port ${PORT}`);
